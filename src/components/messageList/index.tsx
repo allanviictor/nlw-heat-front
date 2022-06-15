a4b70d5142adcd98d827314a8c100dfab0ea03a3
allan victor <allan_victor98@hotmail.com>
2022-06-15 12:40:58 -0300

    lista de mensagens

--- src/components/messageList/index.tsx
+++ src/components/messageList/index.tsx
@@ -1,7 +1,56 @@
-export function MessageList(){
-    return(
+import styles from './Style.module.sass'
+import logo from '../../assets/ic-dowhile.png'
+import { api } from '../../api/api'
+import { useEffect, useState } from 'react'
+
+type Messages = {
+    text: string
+    id: string
+    user: {
+        name: string
+        avatar_url: string
+    }
+}
+
+export function MessageList() {
+
+    const [messages, setMessages] = useState<Messages[]>([])
+
+    useEffect(() => {
+        getMessages()
+    }, [])
+
+    async function getMessages() {
+        await api.get<Messages[]>('/last/messages').then(response => {
+
+            setMessages(response.data)
+        })
+
+    }
+
+
+    return (
         <>
-            <h1>messagelist</h1>
+            <div className={styles.wrapperMessageList}>
+                <img className="logo" src={logo} alt="" />
+                <ul>
+                    {
+                        messages.map(item => {
+                            return (
+                                <li key={item.id}>
+                                    {item.text}
+                                    <div className={styles.userAvatar}>
+                                        <div className={styles.borderGradient}>
+                                            <img src={item.user.avatar_url} alt="" />
+                                        </div>
+                                        {item.user.name}
+                                    </div>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
         </>
     )
 }
